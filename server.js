b1e4733d501d2f87e6443d7dc33bbd291c311617
kumarrahulsindhu2004 <kumarrahulsindhu@gmail.com>
2025-09-18 15:48:43 +0530

    updated origirn

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(cors({
            "https://api-endpoint-imzw.onrender.com",
            "https://your-frontend.onrender.com"
           ], 
-  methods: "GET,POST,PUT,DELETE",
+  methods: ["GET,POST,PUT,DELETE"],
   credentials: true
 }));
 
